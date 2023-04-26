9eee53fb7ccc95025bd0d6c79d4ec047637ea800
Marian K <marian.kloesler@gmx.de>
2023-04-26 18:44:56 +0200

    finished lexer accordingly to first tutorial

--- lexer.ts
+++ lexer.ts
@@ -8,6 +8,10 @@ export enum TokenType {
   Let,
 }
 
+const KEYWORDS: Record<string, TokenType> = {
+  let: TokenType.Let,
+};
+
 export interface Token {
   value: string;
   type: TokenType;
@@ -17,20 +21,69 @@ function token(value = "", type: TokenType): Token {
   return { value, type };
 }
 
+function isAlpha(src: string) {
+  return src.match(/^[A-Za-z]+$/i) !== null;
+}
+
+function isInt(src: string) {
+  return src.match(/^[0-9]+$/i) !== null;
+}
+
+function isSkippable(str: string) {
+  return str == " " || str == "\n" || str == "\t";
+}
+
 export function tokenize(sourceCode: string): Token[] {
   let tokens = new Array<Token>();
   let src = sourceCode.split("");
 
   while (src.length > 0) {
-    if(src[0] =="("){
-        tokens.push(token(src.shift(), TokenType.OpenParen))
-    }else if(src[0] ==")"){
-        tokens.push(token(src.shift(), TokenType.CloseParen))
-    }else if(src[0] =="+" || src[0] =="-" || src[0] =="*" || src[0] =="/"){
-        tokens.push(token(src.shift(), TokenType.BinaryOperator))
-    }else if(src[0] =="="){
-        tokens.push(token(src.shift(), TokenType.Equals))
-  }
+    if (src[0] == "(") {
+      tokens.push(token(src.shift(), TokenType.OpenParen));
+    } else if (src[0] == ")") {
+      tokens.push(token(src.shift(), TokenType.CloseParen));
+    } else if (
+      src[0] == "+" ||
+      src[0] == "-" ||
+      src[0] == "*" ||
+      src[0] == "/"
+    ) {
+      tokens.push(token(src.shift(), TokenType.BinaryOperator));
+    } else if (src[0] == "=") {
+      tokens.push(token(src.shift(), TokenType.Equals));
+    } else {
+      console.log("SOURCE: " + src[0]);
+      if (isSkippable(src[0])) {
+        src.shift();
+      } else if (isInt(src[0])) {
+        let num = "";
+        while (isInt(src[0])) {
+          num += src.shift();
+        }
+        tokens.push(token(num, TokenType.Number));
+      } else if (isAlpha(src[0])) {
+        let ident = "";
+        while (src.length > 0 && isAlpha(src[0])) {
+          ident += src.shift();
+        }
 
+        //check for reserved keywords
+        const reserved = KEYWORDS[ident];
+        if (reserved == undefined) {
+          tokens.push(token(ident, TokenType.Identifier));
+        } else {
+          tokens.push(token(ident, reserved));
+        }
+      } else {
+        console.log("Unrecognized character found in source: ", src[0]);
+        Deno.exit(1);
+      }
+    }
+  }
   return tokens;
+}
+
+const source = await Deno.readTextFile("./test.ts");
+for (const token of tokenize(source)){
+  console.log(token);
 }
